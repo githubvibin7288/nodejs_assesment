1e801a4d4dceb52b4867065313b83ac3796b0afe
kpselvaips <109733821+kpselvaips@users.noreply.github.com>
2024-05-19 20:03:33 +0530

    Syntax updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
 var express = require('../../');
 
-var app = module.exports = express()
+var app = module.exports = express();
 
 app.get('/', function(req, res){
   res.send('Hello World');
